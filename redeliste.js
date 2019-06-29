15d3de621d3ace3d842ab7288114a6685e876712
Mirko Westermeier <mirko@westermeier.de>
2019-06-29 19:29:50 +0200

    Sort persons by name

--- redeliste.js
+++ redeliste.js
@@ -7,7 +7,11 @@ new Vue({
     methods: {
         addPerson: function () {
             this.persons.push({name: this.newPerson});
+            this.sortPersons();
             this.newPerson = '';
+        },
+        sortPersons: function () {
+            this.persons.sort((a,b) => a.name.localeCompare(b.name));
         }
     }
 })
